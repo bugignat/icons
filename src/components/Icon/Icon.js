965fdc8f1f27494a4748dceeb959574f11e182ec
ign47ev <ign47ev@ya.ru>
2018-07-09 23:27:04 +0300

    icon size fix

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,56 +1,64 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import classNames from 'classnames';
-import fileDownload from 'react-file-download';
-import Ripple from 'react-ink';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import classNames from "classnames";
+import fileDownload from "react-file-download";
+import Ripple from "react-ink";
 
-import './Icon.css';
+import "./Icon.css";
 
 class Icon extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       visible: false
-    }
+    };
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         visible: true
-      })
-    })
+      });
+    });
   }
 
   download = () => {
-    const {icon, size} = this.props;
-    let fileName = 'ic_' + icon.name + '_' + size + 'px';
-    fileName = fileName.toLowerCase().replace(' ', '_').replace('.', '') + '.svg';
+    const { icon, size } = this.props;
+    let fileName = "ic_" + icon.name + "_" + size + "px";
+    fileName =
+      fileName
+        .toLowerCase()
+        .replace(" ", "_")
+        .replace(".", "") + ".svg";
     const buttonEl = ReactDOM.findDOMNode(this);
-    const svgEl = buttonEl.getElementsByTagName('svg')[0];
-    svgEl.setAttribute('width', size);
-    svgEl.setAttribute('height', size);
+    const svgEl = buttonEl.getElementsByTagName("svg")[0];
+    svgEl.setAttribute("width", size);
+    svgEl.setAttribute("height", size);
     setTimeout(() => {
       fileDownload(svgEl.outerHTML, fileName);
-    }, 500)
+    }, 500);
   };
 
   render() {
-    const {icon, size, delay} = this.props;
+    const { icon, size, delay } = this.props;
     const iconClass = classNames({
-      'icon': true,
-      'iconActive': this.state.visible
+      icon: true,
+      iconActive: this.state.visible
     });
 
     return (
       <button
-        title={icon.category + '/' + icon.name}
+        title={icon.category + "/" + icon.name}
         onClick={this.download}
         className={iconClass}
-        style={{padding: (100 - size) / 2, transitionDelay: delay / 50 + 's'}}>
-        <div dangerouslySetInnerHTML={{__html: icon.svg}}/>
-        <Ripple style={{color: '#2979FF'}}/>
+        style={{ padding: (100 - size) / 2, transitionDelay: delay / 50 + "s" }}
+      >
+        <div
+          className="iconWrapper"
+          dangerouslySetInnerHTML={{ __html: icon.svg }}
+        />
+        <Ripple style={{ color: "#2979FF" }} />
+        {/* <div className="iconName">{icon.name}</div> */}
       </button>
     );
   }
